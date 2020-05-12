edd9fcbee549f08754436e9546655eddd9a3479c
anangmaruf8 <65202941+anangmaruf8@users.noreply.github.com>
2020-05-11 18:54:37 -0700

    Create Joobsheet 2 C++.cpp
    Mengisi dan menampilkan elemen array (dinamis)

---
+++ Joobsheet 2 C++.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main (){
+string x [13];
+cout<<"masukkan nim mahasiswa 1 : ";
+cin>>x[0];
+cout<<"masukkan nim mahasiswa 2 : ";
+cin>>x[1];
+cout<<"masukkan nim mahasiswa 3 : ";
+cin>>x[2];
+cout<<"masukkan nim mahasiswa 4 : ";
+cin>>x[3];
+cout<<"masukkan nim mahasiswa 5 : ";
+cin>>x[4];
+cout<<"masukkan nim mahasiswa 6 : ";
+cin>>x[5];
+cout<<"masukkan nim mahasiswa 7 : ";
+cin>>x[6];
+cout<<"masukkan nim mahasiswa 8 : ";
+cin>>x[7];
+cout<<"masukkan nim mahasiswa 9 : ";
+cin>>x[8];
+cout<<"masukkan nim mahasiswa 10 : ";
+cin>>x[9];
+cout<<"masukkan nim mahasiswa 11 : ";
+cin>>x[10];
+cout<<"masukkan nim mahasiswa 12 : ";
+cin>>x[11];
+cout<<"masukkan nim mahasiswa 13 : ";
+cin>>x[12];
+
+return 0;}
