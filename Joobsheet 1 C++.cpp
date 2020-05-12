b9ce67d6dd3e02d5cc7a1609b601ef6f79b6a2f8
anangmaruf8 <65202941+anangmaruf8@users.noreply.github.com>
2020-05-11 18:53:34 -0700

    Create Joobsheet 1 C++.cpp
    Menampilkan array satu dimensi

---
+++ Joobsheet 1 C++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Nama : Anang ma'ruf'\n";
+	cout<<"Nim : F1B019023\n";
+	cout<<"Kelompok: Kelompok 6\n\n";
+int nim[13]= {10,11,12,13,14,15,16,17,18,19,20,21,22};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+    cout << "Nim ke-6 adalah " << nim[5] << endl;
+    cout << "Nim ke-7 adalah " << nim[6] << endl;
+    cout << "Nim ke-8 adalah " << nim[7] << endl;
+    cout << "Nim ke-9 adalah " << nim[8] << endl;
+    cout << "Nim ke-10 adalah " << nim[9] << endl;
+    cout << "Nim ke-11 adalah " << nim[10] << endl;
+    cout << "Nim ke-12 adalah " << nim[11] << endl;
+    cout << "Nim ke-13 adalah " << nim[12] << endl;
+}
